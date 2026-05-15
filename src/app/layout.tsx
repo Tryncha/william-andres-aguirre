73ff8c1fee154968d83aade74dbc375715d75836
Tryncha <juanes.aguirre26@gmail.com>
2026-05-14 22:40:32 -0500

    analytics and speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { inter } from '../lib/fonts';
 import Aside from '@/src/components/aside';
 import Footer from '@/src/components/footer';
 import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'William Andres Aguirre'
@@ -11,6 +13,8 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="es">
+      <Analytics />
+      <SpeedInsights />
       <body className={`${inter.className} bg-white text-gray-800`}>
         <Aside />
         {children}
